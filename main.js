ff6717e7a8dd2ef9263c329510807df22fdec716
Hamada Assi <1181121@student.birzeit.edu>
2024-02-08 18:33:57 +0200

    "done"

---
+++ main.js
@@ -0,0 +1,44 @@
+const courseContainer = document.getElementById("courses");
+
+const ratingArray = [];
+function createRating(r){
+  let fill = Math.floor(r);
+  for(let i=1; i< 6; i++){
+    if(i<=fill){
+      ratingArray.push("star");
+    }else if(i - fill === 1 && r % 1 !== 0){
+      ratingArray.push("star-half");
+    }
+    else{
+      ratingArray.push("star-outline");
+    }
+  }
+};
+
+fetch('data.json').then(response => response.json()).then(data => {
+    data.map(courseElement => {
+        const box = document.createElement('a');
+        box.href = "index.html";
+        box.id = courseElement.id;
+        box.classList.add('course');
+        createRating(courseElement.rating);
+        box.innerHTML =
+            `
+            <img src="./Logos/${courseElement.image}" alt="${courseElement.topic} course logo" />
+            <div class="course-info">
+              <h4 class="category">${courseElement.category}</h4>
+              <h5 class="topic">${courseElement.topic}</h5>
+              <div class="rating">
+                <ion-icon name="${ratingArray.shift()}"></ion-icon>
+                <ion-icon name="${ratingArray.shift()}"></ion-icon>
+                <ion-icon name="${ratingArray.shift()}"></ion-icon>
+                <ion-icon name="${ratingArray.shift()}"></ion-icon>
+                <ion-icon name="${ratingArray.shift()}"></ion-icon>
+              </div>
+              <p class="name">Author: ${courseElement.name}</p>
+            </div>
+            `
+            courseContainer.appendChild(box);
+    });
+});
+
