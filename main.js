f8385efe0e1a9407805e855ca5adb32db5b498e5
Hamada Assi <1181121@student.birzeit.edu>
2024-02-18 20:30:13 +0200

    ""

--- main.js
+++ main.js
@@ -2,30 +2,30 @@ const courseContainer = document.getElementById("courses");
 
 const ratingArray = [];
 // calculate stars shape
-function createRating(r){
+const createRating = (r)=> {
   let fill = Math.floor(r);
-  for(let i=1; i< 6; i++){
-    if(i<=fill){
+  for (let i = 1; i < 6; i++) {
+    if (i <= fill) {
       ratingArray.push("star");
-    }else if(i - fill === 1 && r % 1 !== 0){
+    } else if (i - fill === 1 && r % 1 !== 0) {
       ratingArray.push("star-half");
     }
-    else{
+    else {
       ratingArray.push("star-outline");
     }
   }
 };
 
 fetch('data.json').then(response => response.json()).then(data => {
-    data.map(courseElement => {
-      // create course card a > img , info ...
-        const box = document.createElement('a');
-        box.href = `./details.html?id=${courseElement.id}`;
-        box.id = courseElement.id;
-        box.classList.add('course');
-        createRating(courseElement.rating);
-        box.innerHTML =
-            `
+  data.forEach(courseElement => {
+    // create course card a > img , info ...
+    const box = document.createElement('a');
+    box.href = `./details.html?id=${courseElement.id}`;
+    box.id = courseElement.id;
+    box.classList.add('course');
+    createRating(courseElement.rating);
+    box.innerHTML =
+      `
             <img src="./Logos/${courseElement.image}" alt="${courseElement.topic} course logo" class="img-in-box"/>
             <div class="course-info">
               <h4 class="category">${courseElement.category}</h4>
@@ -40,7 +40,7 @@ fetch('data.json').then(response => response.json()).then(data => {
               <p class="name">Author: ${courseElement.name}</p>
             </div>
             `
-            courseContainer.appendChild(box); // push each course data into main courses container
-    });
+    courseContainer.appendChild(box); // push each course data into main courses container
+  });
 });
 
