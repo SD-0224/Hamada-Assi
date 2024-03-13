d1474d23d13ef4f526965d0cb206cfed2114e25b
Hamada Assi <1181121@student.birzeit.edu>
2024-03-13 14:14:07 +0200

    clean

--- js_Files/HTML_Rendering/renderBtnText.js
+++
@@ -1,6 +0,0 @@
-import { isFavorites } from "../shared/mainFunctions.js";
-
-export function renderBtnText(id) {
-    let btnText = isFavorites(id) ? "Remove from Favorites" : `Add to Favorites <ion-icon name="heart-outline" class="icon add-myFavorites"></ion-icon>`;
-    return btnText;
-}
